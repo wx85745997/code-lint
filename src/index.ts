6f8b0de6ded0eff983b375ee8acdc351736b6960
wx85745997 <497045233@qq.com>
2021-06-09 21:24:22 +0800

    2
    Signed-off-by: wx85745997 <497045233@qq.com>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
-import path from 'path'
-import fs from 'fs-extra';
+import path from "path"
+import fs from "fs-extra"
+import ora from "ora"
 
-export const init=()=>{
-  console.log('哈哈')
+import { installEditorconfig } from "./tasks/editorconfig"
+
+export const init = () => {
+  const pkgPath = path.join(process.cwd(), "package.json")
+  const pkg = fs.readJsonSync(pkgPath)
+
+  if (!fs.existsSync(pkgPath)) {
+    throw new Error(`can't find package.json in ${process.cwd()}`)
+  }
+
+  const spinner = ora(`Install ${pkg.name}`).start()
+
+  const startTime = Date.now()
+
+  const addFileTasks: Array<[string, string]> = []
 }
---
+++ src/tasks/editorconfig.ts
@@ -0,0 +1,5 @@
+export const installEditorconfig=()=>{
+  return {
+    addFiles:['./config/.editorconfig','./.editorconfig']
+  }
+}
